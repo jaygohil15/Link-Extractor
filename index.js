a4a87b997ba2b3d8681be5f5de03ad64f3017292
Jay <jaygohil15111999@gmail.com>
2022-01-11 19:42:16 +0530

    JS file served statically

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
    console.log('get req')
    res.sendFile('./link_extractor.html', {root: __dirname})
