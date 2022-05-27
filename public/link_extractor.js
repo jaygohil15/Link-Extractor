932d90d637d6120b7172f7d5eac05b424cdd7e05
Jay <jaygohil15111999@gmail.com>
2022-05-27 16:12:07 +0530

    Opening of limited feature added

--- public/link_extractor.js
+++ public/link_extractor.js
@@ -1,6 +1,8 @@
+
+const urlRegex =
+  /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+
 const openLinks = (event) => {
-  const urlRegex =
-    /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
   event.preventDefault();
 
@@ -8,9 +10,33 @@ const openLinks = (event) => {
   let links = str.match(urlRegex);
   if (!links) {
     console.log("No links Found");
-  }
-  for (let link of links) {
-     console.log(link)
-     window.open(link, '_blank')
+  } else {
+    for (let link of links) {
+      console.log(link)
+      window.open(link, '_blank')
+    }
   }
 };
+
+const open10Link = () => {
+
+  let counter = document.getElementById('counter')
+  let str = document.getElementById("pastedText").value;
+  let links = str.match(urlRegex);
+  if (!links) {
+    console.log("No links Found");
+    alert("No Links Found in text")
+  } else {
+    for (let i = 0; i < 10; i++) {
+      console.log(links[parseInt(counter.textContent)])
+      if (parseInt(counter.textContent) >= links.length) {
+        counter.innerHTML = 0
+        document.getElementById('warn').innerHTML = "Done"
+        break
+      }
+      window.open(links[parseInt(counter.textContent)], '_blank')
+      counter.innerHTML = parseInt(counter.textContent) + 1
+      console.log(counter)
+    }
+  }
+}
